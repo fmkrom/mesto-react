009cf1b5e0487438b96d862e7244f6f63c0af96f
fmkrom <fmkrom@yandex.ru>
2021-03-24 10:20:43 +0300

    clear repository

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <section className="footer">
-           <p className="footer__copyright">&copy; 2020 Ефим Романенко</p>
-        </section>
-    )
-};
-
-export default Footer;
